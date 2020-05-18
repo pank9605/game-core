8f91b2889a84ac745d1bae266ca430667c41b2e3
Eduardo Chavez <pank9605@hotmail.com>
2020-05-18 17:32:10 -0500

    Ajustes de botones sociales

--- resources/views/general/news.blade.php
+++ resources/views/general/news.blade.php
@@ -25,7 +25,7 @@
                                  data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Compartir</a>
                             </div>--}}
                             <div class="col-auto pr-0"><a href="{{url('http://www.gcgamecore.com/news/'.$news->category->name.'/'.$news->clasification->name.'/'.$news->id)}}" class="twitter-share-button" data-size="large" data-show-count="false">Tweet</a></div>
-                            <div class="col-auto fb-like pl-md-1" data-href="{{url('http://www.gcgamecore.com/news/'.$news->category->name.'/'.$news->clasification->name.'/'.$news->id)}}" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="true"></div>
+                            <div class="col-auto fb-like pl-1" data-href="{{url('http://www.gcgamecore.com/news/'.$news->category->name.'/'.$news->clasification->name.'/'.$news->id)}}" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="true"></div>
                         </div>
 
                         <p>{{$news->introduction}}.</p>
@@ -99,7 +99,7 @@
                      data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Compartir</a>
                 </div>--}}
                 <div class="col-auto pr-0"><a href="{{url('http://www.gcgamecore.com/news/'.$news->category->name.'/'.$news->clasification->name.'/'.$news->id)}}" class="twitter-share-button" data-size="large" data-show-count="false">Tweet</a></div>
-                <div class="col-auto fb-like pl-md-1" data-href="{{url('http://www.gcgamecore.com/news/'.$news->category->name.'/'.$news->clasification->name.'/'.$news->id)}}" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="true"></div>
+                <div class="col-auto fb-like pl-1" data-href="{{url('http://www.gcgamecore.com/news/'.$news->category->name.'/'.$news->clasification->name.'/'.$news->id)}}" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="true"></div>
             </div>
 
             <div class="fb-comments mb-2" data-mobile="Auto-detected" data-href="{{url('http://www.gcgamecore.com/news/'.$news->category->name.'/'.$news->clasification->name.'/'.$news->id)}}" data-numposts="10" data-width="100%"></div>
