4a0d001473b60146cbbc91e48d58d75cda93191f
Eduardo Chavez <pank9605@hotmail.com>
2020-05-19 17:51:15 -0500

    app facebook

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,26 +49,6 @@
 <body class="p-0 m-0">
 
 
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : '260127125344466',
-            xfbml      : true,
-            version    : 'v7.0'
-        });
-        FB.AppEvents.logPageView();
-    };
-
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-
-
 <div id="fb-root"></div>
 <script defer>(function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
